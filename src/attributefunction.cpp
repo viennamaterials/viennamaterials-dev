1eb27ec8cfc63551e394fde016b70a6635371fed
matthias <matthias@mint-vm.local>
2014-07-20 19:56:35 +0200

    implement xpath wrapper

--- src/attributefunction.cpp
+++ src/attributefunction.cpp
@@ -10,6 +10,34 @@
    license:    see file LICENSE in the base directory
 ============================================================================= */
 
+#include "viennamaterials/attributefunction.h"
 
+long viennamaterials::IAttributeFunction::qeuery_number_of_attributes(viennamaterials::library_handle& lib, std::string& xpath_query)
+{
+  std::string query = "count(" + xpath_query + "/@*)";
+  return lib->query_xpath_number(query);
+}
 
+std::string viennamaterials::IAttributeFunction::query_attribute_name(viennamaterials::library_handle& lib, std::string& xpath_query, long position)
+{
+  //TODO: check validity position?
+  std::ostringstream position_string;
+  position_string << position;
+  std::string query = "name(" + xpath_query + "/@*[" + position_string.str() + "])";
+  return lib->query_xpath_string(query);
+}
 
+std::string viennamaterials::IAttributeFunction::query_attribute(viennamaterials::library_handle& lib, std::string& xpath_query, long position)
+{
+  //TODO: check validity position?
+  std::ostringstream position_string;
+  position_string << position;
+  std::string query = "string(" + xpath_query + "/@*[" + position_string.str() + "])";
+  return lib->query_xpath_string(query);
+}
+
+std::string viennamaterials::IAttributeFunction::query_attribute(viennamaterials::library_handle& lib, std::string& xpath_query, std::string attribute_name)
+{
+  std::string query = "string(" + xpath_query + "/@" + attribute_name + ")";
+  return lib->query_xpath_string(query);
+}
