e402b60fba2a31e2f31361dae1ad9146d7912707
matthias <matthias@mint-vm.local>
2014-07-28 19:45:47 +0200

    testing: code for testing

--- examples/tutorial/functionbackend-demo.cpp
+++ examples/tutorial/functionbackend-demo.cpp
@@ -14,7 +14,13 @@
 //
 #include "viennamaterials/platform.hpp"
 
-#include "viennamaterials/attributefunctionproxy.h"
+//#include "viennamaterials/attributefunctionproxy.h"
+
+//WIP:
+#include "viennamaterials/xmlvalueentity.hpp"
+#include "viennamaterials/xmlvaluescalar.hpp"
+
+#include "viennamaterials/functionbackendpython.hpp"
 
 int main(int argc, char * argv[])
 {
@@ -39,19 +45,43 @@ int main(int argc, char * argv[])
 
   std::string query = "/*/*[id='test-material']/*[id='parameter2']/function";
 
-  viennamaterials::attribute_function_proxy python_demo;
-
-  python_demo.init(matlib, query, viennamaterials::python);
-
-  std::vector<viennamaterials::FunctionArgumentBase> args;
-  python_demo.evaluate(args);
-
-  python_demo.deinit();
-
-
+//  viennamaterials::attribute_function_proxy python_demo;
+//
+//  python_demo.init(matlib, query, viennamaterials::python);
+//
+//  std::vector<viennamaterials::FunctionArgumentBase> args;
+//  python_demo.evaluate(args);
+//
+//  python_demo.deinit();
 
 
 
+  //testing area
 
+//  viennamaterials::xml_value_scalar_integer testobj;
+  viennamaterials::xml_value_entity* testobj = new viennamaterials::xml_value_scalar_integer;
+  std::string name = "test object";
+  testobj->set_name(name);
+  testobj->set_type(viennamaterials::scalar_int);
+
+  std::cout << "name: " << testobj->get_name() << std::endl;
+  std::cout << "type: " << testobj->get_type() << std::endl;
+
+  int* ptr = new int(12);
+  testobj->set_value(ptr);
+
+  std::cout << "value: " << *(int*)testobj->get_value_ptr() << std::endl;
+
+
+  std::string code = "def func(x,y): \n print x*y \n return 2.0";
+  std::string func_name = "func";
+  viennamaterials::function_backend_python *demo = new viennamaterials::function_backend_python;
+  demo->init(code, func_name);
+  demo->evaluate();
+  delete demo;
+
+
+
+  std::cout << "end of program" << std::endl;
   return EXIT_SUCCESS;
 }
