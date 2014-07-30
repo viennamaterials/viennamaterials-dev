96b2b8f429f633ef8f8e0b9d36e4273767f80389
matthias <matthias@mint-vm.local>
2014-07-30 10:34:24 +0200

    testing: code for testing

--- examples/tutorial/functionbackend-demo.cpp
+++ examples/tutorial/functionbackend-demo.cpp
@@ -40,19 +40,18 @@ int main(int argc, char * argv[])
   viennamaterials::library_handle matlib = viennamaterials::generator(filename);
 
 
-
-
-
   std::string query = "/*/*[id='test-material']/*[id='parameter2']/function";
 
 
 
-  //testing area
+  /** Testing area **/
 
+  /**
+   * Xml_value_entity
+   */
   viennamaterials::xml_value_entity* value_ptr = new viennamaterials::xml_value_scalar_integer;
   std::string name = "test object";
   value_ptr->set_name(name);
-  value_ptr->set_type(viennamaterials::type_int);
 
   std::cout << "name: " << value_ptr->get_name() << std::endl;
   std::cout << "type: " << value_ptr->get_type() << std::endl;
@@ -60,21 +59,45 @@ int main(int argc, char * argv[])
   long val = 12;
   value_ptr->set_value(val);
 
-  viennamaterials::tag_scalar_int tag;
-  std::cout << "value: " << value_ptr->get_value(tag) << std::endl;
+  viennamaterials::tag_scalar_int tag_int;
+  std::cout << "value: " << value_ptr->get_value(tag_int) << std::endl;
 
 
+  /**
+   * Python function backend
+   */
+
+  /// Define python funtion
   std::cout << "python backend demo:" << std::endl;
-  std::string code = "def func(x,y): \n print x*y \n return 2.0";
+  std::string code = "def func(x,y): \n print x*y \n return 2.2";
   std::string func_name = "func";
+
   viennamaterials::function_backend *demo = new viennamaterials::function_backend_python;
   demo->init(code, func_name);
-  demo->eval(tag);
+
+  /// Define arguments
+  std::vector<viennamaterials::xml_value_entity*> args;
+  viennamaterials::xml_value_entity* arg;
+  arg = new viennamaterials::xml_value_scalar_float;
+  arg->set_index(0);
+  arg->set_value(3.0);
+  args.push_back(arg);
+  arg = new viennamaterials::xml_value_scalar_float;
+  arg->set_index(1);
+  arg->set_value(0.5);
+  args.push_back(arg);
+
+  viennamaterials::tag_scalar_float tag_float;
+  double result = demo->eval(tag_float, args);
+  std::cout << "result: " << result << std::endl;
 
 
+  /**
+   * Attribute entity
+   */
   std::cout << "attribute entity demo:" << std::endl;
-  viennamaterials::attribute_entity* attr_ptr = new viennamaterials::attribute_entity_function(viennamaterials::function_int, demo);
-  std::cout << "result: " << attr_ptr->eval(tag) << std::endl;
+  viennamaterials::attribute_entity* attr_ptr = new viennamaterials::attribute_entity_function(viennamaterials::function_int, demo, args);
+  std::cout << "result: " << attr_ptr->eval(tag_int) << " <-- attribute entity used as int-function" << std::endl;
 
   if(attr_ptr->is_function_int()) /// using method name
     std::cout << "is_function_int" << std::endl;
