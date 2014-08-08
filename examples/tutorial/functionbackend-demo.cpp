7d12b196a4f484e6440a7baf0b51d561e8dafc7e
matthias <matthias@mint-vm.local>
2014-08-08 19:51:25 +0200

    cleanup for broker demo

--- examples/tutorial/functionbackend-demo.cpp
+++ examples/tutorial/functionbackend-demo.cpp
@@ -12,16 +12,6 @@
 
 // ViennaMaterials includes
 //
-#include "viennamaterials/platform.hpp"
-
-//WIP:
-#include "viennamaterials/xmlvalueentity.hpp"
-#include "viennamaterials/xmlvaluescalar.hpp"
-
-#include "viennamaterials/functionbackendpython.hpp"
-
-#include "viennamaterials/attributeentityfunction.hpp"
-
 #include "viennamaterials/broker.hpp"
 
 int main(int argc, char * argv[])
@@ -38,125 +28,58 @@ int main(int argc, char * argv[])
   /** A string-path to a test material xml file **/
   std::string filename(argv[1]);
 
-  /** Import the file and create a material library object **/
-  viennamaterials::library_handle matlib = viennamaterials::generator(filename);
+  /** Create broker object **/
+  viennamaterials::broker broker(filename);
+
+  std::string query;
+  viennamaterials::attribute_handle attribute;
+
+  std::cout << "########################  Scalar attributes  ########################" << std::endl;
+
+  /** Load boolean scalar from material library **/
+  query = "/*/*[id='test-material']/*[id='bool-scalar']";
+  attribute = broker.query(query);
+  if(attribute->is_scalar_bool())
+    std::cout << "boolean scalar value:          " << attribute->evaluate<bool>() << std::endl;
+
+  /** Load integer scalar from material library **/
+  query = "/*/*[id='test-material']/*[id='int-scalar']";
+  attribute = broker.query(query);
+  if(attribute->is_scalar_int())
+    std::cout << "integer scalar value:          " << attribute->evaluate<long>() << std::endl;
+
+  /** Load floating point scalar from material library **/
+  query = "/*/*[id='test-material']/*[id='float-scalar']";
+  attribute = broker.query(query);
+  if(attribute->is_scalar_float())
+    std::cout << "floating point scalar value:   " << attribute->evaluate<double>() << std::endl;
 
 
-  std::string query = "/*/*[id='test-material']/*[id='parameter2']";
+  std::cout << "######################## Function attributes ########################" << std::endl;
 
-
-
-  /** Testing area **/
-
-  /**
-   * Xml_value_entity
-   */
-  viennamaterials::shared_ptr<viennamaterials::xml_value_entity> value_ptr(new viennamaterials::xml_value_scalar_integer);
-  std::string name = "test object";
-  value_ptr->set_name(name);
-
-  std::cout << "name: " << value_ptr->get_name() << std::endl;
-  std::cout << "type: " << value_ptr->get_type() << std::endl;
-
-  long val = 12;
-  value_ptr->set_value(val);
-
-  std::cout << "value: " << value_ptr->get_value<long>() << std::endl;
-
-
-  /**
-   * Python function backend
-   */
-
-  /// Define python funtion
-  std::cout << std::endl << "python backend demo:" << std::endl;
-  std::string code = "def func(x,y): \n print x*y \n return 2.2";
-  std::string func_name = "func";
-
-  viennamaterials::function_backend_handle demo(new viennamaterials::function_backend_python);
-  demo->init(code, func_name);
-
-  /// Define arguments
-  std::vector<viennamaterials::xml_value_entity_handle> args;
-  {
-    viennamaterials::xml_value_entity_handle arg(new viennamaterials::xml_value_scalar_float);
-    arg->set_index(0);
-    arg->set_value(3.0);
-    args.push_back(arg);
-  }
-  {
-    viennamaterials::xml_value_entity_handle arg(new viennamaterials::xml_value_scalar_float);
-    arg->set_index(1);
-    arg->set_value(0.5);
-    args.push_back(arg);
-  }
-
-  viennamaterials::tag_scalar_float tag_float;
-  double result = demo->eval(tag_float, args);
-  std::cout << "result: " << result << std::endl;
-
-
-  /**
-   * Attribute entity
-   */
-  std::cout << std::endl << "attribute entity demo:" << std::endl;
-  viennamaterials::shared_ptr<viennamaterials::attribute_entity> attr_ptr(new viennamaterials::attribute_entity_function(viennamaterials::function_int, demo, args));
-  std::cout << "result: " << attr_ptr->evaluate<long>() << " <-- attribute entity used as int-function" << std::endl;
-
-  if(attr_ptr->is_function_int()) /// using method name
-    std::cout << "is_function_int" << std::endl;
-
-  viennamaterials::tag_function_int func_tag;
-  if(attr_ptr->is_function(func_tag))  /// using tag dispatching
-    std::cout << "is_function" << std::endl;
-
-  if(attr_ptr->is_type(viennamaterials::function_int))  /// using enum argument
-    std::cout << "is_type" << std::endl;
-
-
-
-
-
-  /**
-   * Broker
-   */
-  std::cout << std::endl << "broker demo:" << std::endl;
-  viennamaterials::broker broker_demo(filename);
-  viennamaterials::attribute_handle attribute = broker_demo.query(query);
+  /** Load function with float return type from material library **/
+  query = "/*/*[id='test-material']/*[id='float-function-multiply']";
+  attribute = broker.query(query);
   if(attribute->is_function_float())
   {
-    double result = attribute->evaluate<double>();
-    std::cout << "eval: " << result << std::endl;
+    /// Using default values of function arguments
+    std::cout << "multiply function (defaults):  " << attribute->evaluate<double>() << std::endl;
 
+    /// Set value for second function argument and evaluate
     std::vector<viennamaterials::xml_value_entity_handle> func_args = attribute->get_dependencies();
     func_args[1]->set_value(0.125);
     attribute->set_dependencies(func_args);
-    std::cout << "eval: " << attribute->evaluate<viennamaterials::xml_float>() << std::endl;
-  }
-
-  /// scalar attribute
-  std::cout << std::endl << "broker demo - scalar:" << std::endl;
-  query = "/*/*[id='test-material']/*[id='parameter1']";
-  viennamaterials::attribute_handle scalar_attribute = broker_demo.query(query);
-  if(scalar_attribute->is_scalar_float())
-  {
-    double result = scalar_attribute->evaluate<double>();
-    std::cout << "eval: " << result << std::endl;
-  }
-  if(scalar_attribute->is_scalar_int())
-  {
-    long result = scalar_attribute->evaluate<long>();
-    std::cout << "eval: " << result << std::endl;
-  }
-  if(scalar_attribute->is_scalar_bool())
-  {
-    bool result = scalar_attribute->evaluate<bool>();
-    std::cout << "eval: " << result << std::endl;
+    std::cout << "multiply function (arg set):   " << attribute->evaluate<double>() << std::endl;
   }
 
 
+  /*
+   * TODO
+   * reference argument
+   * function argument
+   */
 
 
-  std::cout << std::endl << "end of program" << std::endl;
+  std::cout << "########################     End of demo     ########################" << std::endl;
   return EXIT_SUCCESS;
 }
