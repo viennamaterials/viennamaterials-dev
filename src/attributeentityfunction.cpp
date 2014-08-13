414db7914fb372ec0bfe76c4c79d82f22c2b29d4
matthias <matthias@mint-vm.local>
2014-08-13 16:38:45 +0200

    refine implementation of set_dependencies

--- src/attributeentityfunction.cpp
+++ src/attributeentityfunction.cpp
@@ -10,8 +10,9 @@
    license:    see file LICENSE in the base directory
 ============================================================================= */
 
-#include "../viennamaterials/attributeentityfunction.hpp"
-#include "../viennamaterials/exceptions.hpp"
+#include "viennamaterials/attributeentityfunction.hpp"
+#include "viennamaterials/exceptions.hpp"
+#include <algorithm>
 
 namespace viennamaterials
 {
@@ -28,11 +29,41 @@ std::vector<xml_value_entity_handle> attribute_entity_function::get_dependencies
   return args_;
 }
 
+struct arg_comperator
+{
+  explicit arg_comperator(const std::string& name) { name_ = name; }
+  inline bool operator()(const xml_value_entity_handle& obj) const
+  {
+    if(obj->get_name().compare(name_) == 0)
+      return true;
+    return false;
+  }
+private:
+  std::string name_;
+};
+
 void attribute_entity_function::set_dependencies(std::vector<xml_value_entity_handle>& args)
 {
-  //TODO ? cope with misordered vector?
-  //TODO ? cope with wrong vector (size, elements)?
-  args_ = args;
+  for(std::vector<xml_value_entity_handle>::iterator function_arg_iter = args_.begin(); function_arg_iter != args_.end(); ++function_arg_iter)
+  {
+    std::string name = (*function_arg_iter)->get_name();
+    std::vector<xml_value_entity_handle>::iterator passed_arg_iter = std::find_if(args.begin(), args.end(), arg_comperator(name));
+
+    switch((*passed_arg_iter)->get_type())
+    {
+      case scalar_bool:
+        (*function_arg_iter)->set_value((*passed_arg_iter)->get_value<xml_bool>());
+        break;
+      case scalar_int:
+        (*function_arg_iter)->set_value((*passed_arg_iter)->get_value<xml_int>());
+        break;
+      case scalar_float:
+        (*function_arg_iter)->set_value((*passed_arg_iter)->get_value<xml_float>());
+        break;
+      default:
+        throw attribute_entity_error("Invalid argument type encountered");
+    }
+  }
 }
 
 xml_bool attribute_entity_function::eval(tag_scalar_bool tag)
