be98b9916d5d1dc98f6d1435762572f2b9a54d23
Josef Weinbub <weinbub@iue.tuwien.ac.at>
2014-09-30 10:26:33 +0200

    tested germanium and silicon lattice mobility client server

--- examples/tutorial/client.py
+++ examples/tutorial/client.py
@@ -1,6 +1,7 @@
 import sys
 import socket
 import pyviennamaterials
+import csv
 
 mySocket = socket.socket ( socket.AF_INET, socket.SOCK_STREAM )
 
@@ -14,7 +15,7 @@ except socket.error, msg:
   print "[Client][Error] server is not reachable:",host,":",port
   sys.exit(-1)
 
-query='/*/*[id=\'Si\']/*[id=\'mu_L_n\']'
+query='/*/*[id=\'Ge\']/*[id=\'mu_L_n\']'
 
 mySocket.sendall(query)
 
@@ -29,3 +30,20 @@ func_args = attribute.get_dependencies()
 func_args[0].set_value(400.)
 attribute.set_dependencies(func_args)
 print "float function value new: " , attribute.evaluate_value_float()
+
+# Now, use our standalone model retrieved from the server to generate a
+# figure by sweeping through a range of values and evaluate the model for each
+# single one of them
+
+data_table = list()
+for T in range(100,510,10):
+  func_args[0].set_value(float(T))
+  attribute.set_dependencies(func_args)
+  data_table.append([T, attribute.evaluate_value_float()])
+#print data_table
+
+#write the data table to a CSV file
+with open('output.csv', 'wb') as csvfile:
+  writer = csv.writer(csvfile, delimiter=' ')
+  for row in data_table:
+    writer.writerow(row)
